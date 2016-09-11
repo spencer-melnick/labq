f37b83aaf1d2c3e34576aaaf394d58595117e0a8
spencer-melnick <smelnick97@gmail.com>
2016-09-11 18:24:12 -0400

    Implemented more of the FlapperNews sample app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,14 +1,36 @@
 var app = angular.module('flapperNews', []);
 
+app.factory('posts', [function(){
+  var obj = {
+    posts: [{title: 'test post, do not reply', link: '', upvotes: 0}]
+  };
+
+  return obj;
+}]);
+
 app.controller('MainCtrl', [
-'$scope',
-function($scope){
-  $scope.test = 'Hello world!';
-  $scope.posts = [
-    {title: 'post 1', upvotes: 5},
-    {title: 'post 2', upvotes: 2},
-    {title: 'post 3', upvotes: 15},
-    {title: 'post 4', upvotes: 9},
-    {title: 'post 5', upvotes: 4}
-  ];
+  '$scope',
+  'posts',
+  function($scope, posts){
+    $scope.test = 'Hello world!';
+    $scope.posts = posts.posts;
+
+    console.log(posts);
+
+    $scope.addPost = function(){
+      if (!$scope.title || $scope.title === '')
+        return;
+
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+      $scope.title = '';
+      $scope.link = '';
+    }
+
+    $scope.incrementUpvotes = function(post) {
+    post.upvotes += 1;
+  };
 }]);
