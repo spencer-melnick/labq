54fbb179858f91228e0b02448dacf66fd0537309
Redesigner <stephenwmelnick@gmail.com>
2016-09-14 19:56:24 -0400

    Added ability to make comments!
    ^

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -49,6 +49,15 @@ app.controller('PostsCtrl', [
     $scope.incrementUpvotes = function(comment) {
       comment.upvotes += 1;
     };
+    $scope.addComment = function(){
+      if($scope.body === '') { return; }
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    };
   }
 ]);
 
